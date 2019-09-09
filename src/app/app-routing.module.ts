c9d0ce3d3af9e560c6b47f06a5705d1fb7327ad8
talitast <talitatravassos.tt@gmail.com>
2019-09-09 15:06:15 -0300

    routes finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: HomeComponent 
   },
   { 
-    path: "edit", 
+    path: "edit/:id", 
     component: EditUserComponent 
   }
 ];
--- src/app/components/edit-user/edit-user.component.ts
+++ src/app/components/edit-user/edit-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'edit-user',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap
+      .subscribe( param => {
+        console.log(param)
+    })
   }
 
 }
